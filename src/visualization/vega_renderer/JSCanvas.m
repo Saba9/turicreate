f939709be679c5af99520b67aa850170ae54c65e
Saba Noorassa <saba_n@apple.com>
2019-07-18 18:36:01 -0700

    Change VegaRenderer to expose canvas through DOM API

--- src/visualization/vega_renderer/JSCanvas.m
+++ src/visualization/vega_renderer/JSCanvas.m
@@ -172,18 +172,21 @@
 }
 
 - (void)resizeWithWidth:(double)width height:(double)height {
-    if (width > 0 && height > 0) {
+    // This is a hack to ensure that neither width or height is ever zero as this causes
+    // CGLayerCreateWithContext(...) to crash. This can occur when width and height are
+    // set one at a time in subsequent calls. A more faithful rendering context implementation
+    // would wait until both are set before creating the layer.
+    width = MAX(1, width);
+    height = MAX(1, height);
 
-        // Because we are about to create a new layer, set up other relevant
-        // instance properties so they are in the right state.
-        _currentTransform = CGAffineTransformIdentity;
-        assert(self != nil);
-        CGLayerRef layer = CGLayerCreateWithContext(_parentContext, CGSizeMake(width, height), nil);
-        self.layer = layer;
-        CGLayerRelease(layer);
-        CGContextConcatCTM(self.context, [self.class flipYAxisWithHeight:height]);
-
-    }
+    // Because we are about to create a new layer, set up other relevant
+    // instance properties so they are in the right state.
+    _currentTransform = CGAffineTransformIdentity;
+    assert(self != nil);
+    CGLayerRef layer = CGLayerCreateWithContext(_parentContext, CGSizeMake(width, height), nil);
+    self.layer = layer;
+    CGLayerRelease(layer);
+    CGContextConcatCTM(self.context, [self.class flipYAxisWithHeight:height]);
 }
 
 - (double)width {
@@ -202,14 +205,10 @@
     [self resizeWithWidth:self.width height:height];
 }
 
-- (instancetype)initWithWidth:(double)width
-                       height:(double)height
-                      context:(CGContextRef)parentContext {
+- (instancetype)initWithContext:(CGContextRef)parentContext {
     self = [super init];
     _layer = nil;
     _parentContext = parentContext;
-    [self resizeWithWidth:width height:height];
-    assert(CFGetRetainCount(_layer) == 1);
     return self;
 }
 
@@ -293,7 +292,7 @@
     if(newFont == nil) {
         newFont = [NSFont systemFontOfSize:fontSize];
         // TODO should we be updating _font to reflect the system font we've fallen back to?
-        NSLog(@"The specified font: '%@' is unavailable. Falling back to '%@'.", fontStr, [newFont displayName]);
+        NSLog(@"warning: the specified font: '%@' is unavailable. Falling back to '%@'.", fontStr, [newFont displayName]);
     } else {
         _font = fontStr;
     }
@@ -634,7 +633,11 @@
     NSArray *args = [JSContext currentArguments];
     (void)args;
     assert(args != nil);
-    assert(args.count == 0);
+    if(args.count > 0) {
+        // Vega occasionally makes calls to fill with four arguments when zero are expected.
+        // Might have intended to call fillRect instead.
+        NSLog(@"warning: 'fill' called with %lu arguments when none were expected", args.count);
+    }
     CGPathRef currentPath = CGContextCopyPath(self.context);
     if ([_fillStyle isKindOfClass:VegaCGLinearGradient.class]) {
         VegaCGLinearGradient *gradient = _fillStyle;
@@ -658,11 +661,9 @@
 
 @implementation VegaCGCanvas
 
-- (instancetype)initWithWidth:(double)width
-                       height:(double)height
-                      context:(CGContextRef)parentContext {
+- (instancetype)initWithContext:(CGContextRef)parentContext {
     self = [super init];
-    self.context = [[VegaCGContext alloc] initWithWidth:width height:height context:parentContext];
+    self.context = [[VegaCGContext alloc] initWithContext:parentContext];
     return self;
 }
 
